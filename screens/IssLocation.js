004c45034f25da27b53a9b4277b45bbe56eddf59
ThalyaCuin <thalyacuin@gmail.com>
2023-01-16 22:27:18 -0300

    aula78

--- screens/IssLocation.js
+++ screens/IssLocation.js
@@ -1,17 +1,146 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import {
+    Text,
+    View,
+    StyleSheet,
+    ImageBackground,
+    StatusBar,
+    SafeAreaView,
+    Image,
+    Alert,
+    Platform
+} from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import axios from "axios";
 
 export default class IssLocationScreen extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            location: {},
+        };
+    }
+
+    componentDidMount() {
+        this.getIssLocation()
+    }
+
+/*Vamos escrever uma função chamada getIssLocation()
+(obter localização da EEI), na qual podemos usar axios se
+quisermos buscar dados de uma fonte externa. Aqui,
+estamos usando axios para solicitar que a API fornecida
+busque os dados e defina os dados retornados em um
+estado chamado de location (localização).*/
+
+    getIssLocation = () => {
+        axios
+            .get("https://api.wheretheiss.at/v1/satellites/25544")
+            .then(response => {
+                this.setState({ location: response.data })
+            })
+            .catch(error => {
+                Alert.alert(error.message)
+            })
+    }
+
     render() {
-        return (
-            <View
-                style={{
-                    flex: 1,
-                    justifyContent: "center",
-                    alignItems: "center"
-                }}>
-                <Text>Tela de Localização da EEI!</Text>
-            </View>
-        )
+
+/*Object.Keys() é um método JavaScript amplamente utilizado que retorna uma
+matriz com todas as chaves de um objeto  estamos passando
+this.state.location dentro de nossa função Object.keys() e verificando se o tamanho
+das chaves é 0 ou não. Se for 0, exibimos a mensagem “Carregando”; caso contrário,
+simplesmente exibimos o conteúdo da tela.
+*/
+
+        if (Object.keys(this.state.location).length === 0) {
+            return (
+                <View
+                    style={{
+                        flex: 1,
+                        justifyContent: "center",
+                        alignItems: "center"
+                    }}>
+                    <Text>Carregando</Text>
+                </View>
+            )
+        } else {
+            return (
+                <View style={styles.container}>
+                    <SafeAreaView style={styles.droidSafeArea} />
+                    <ImageBackground source={require('../assets/bg.png')} style={styles.backgroundImage}>
+                        <View style={styles.titleContainer}>
+                            <Text style={styles.titleText}>Localização da EEI</Text>
+                        </View>
+                        <View style={styles.mapContainer}>
+                            <MapView //O MapView irá nos ajudar a adicionar o mapa
+                                style={styles.map}
+                                region={{
+                                    latitude: this.state.location.latitude,
+                                    longitude: this.state.location.longitude,
+                                    latitudeDelta: 100,
+                                    longitudeDelta: 100
+                                }}
+                            >
+                                <Marker //Marker irá nos ajudar a marcar a localização no mapa.
+
+                                    coordinate={{ latitude: this.state.location.latitude, longitude: this.state.location.longitude }}
+                                >
+                                    <Image source={require('../assets/iss_icon.png')} style={{ height: 50, width: 50 }} />
+                                </Marker>
+                            </MapView>
+                        </View>
+                        <View style={styles.infoContainer}>
+                            <Text style={styles.infoText}>Latitude: {this.state.location.latitude}</Text>
+                            <Text style={styles.infoText}>Longitude: {this.state.location.longitude}</Text>
+                            <Text style={styles.infoText}>Altitude (KM): {this.state.location.altitude}</Text>
+                            <Text style={styles.infoText}>Velocidade (KM/H): {this.state.location.velocity}</Text>
+                        </View>
+                    </ImageBackground>
+                </View>
+            )
+        }
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    droidSafeArea: {
+        marginTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
+    },
+    backgroundImage: {
+        flex: 1,
+        resizeMode: 'cover',
+    },
+    titleContainer: {
+        flex: 0.1,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    titleText: {
+        fontSize: 30,
+        fontWeight: "bold",
+        color: "white"
+    },
+    mapContainer: {
+        flex: 0.7
+    },
+    map: {
+        width: "100%",
+        height: "100%"
+    },
+    infoContainer: {
+        flex: 0.2,
+        backgroundColor: 'white',
+        marginTop: -10,
+        borderTopLeftRadius: 30,
+        borderTopRightRadius: 30,
+        padding: 30
+    },
+    infoText: {
+        fontSize: 15,
+        color: "black",
+        fontWeight: "bold"
+    }
+})
